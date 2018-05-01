4662a38d98936580b9519503298b19033310d95a
Джанаев Азат <osycle@gmail.com>
2018-05-01 17:46:34 +0500

    01.05

--- app/js/main.js
+++ app/js/main.js
@@ -178,7 +178,22 @@
       })
     })
     //flickityPrevNext( ".productions-carousel" );
-
+    if( $(".short-reviews-carousel .carousel-items figure").length > 1 )
+      $('.short-reviews-carousel .carousel-items').flickity({
+        imagesLoaded: true,
+        autoPlay: false,
+        pauseAutoPlayOnHover: true,
+        arrowShape: arrowStyle,
+        initialIndex: 1,
+        prevNextButtons: false,
+        draggable: true,
+        wrapAround: false,  
+        pageDots: false,
+        contain: false,
+        percentPosition: true,
+        cellAlign: 'center'
+      });
+    flickityPrevNext( $('.short-reviews-carousel') );
 
     // FANCYBOX
     if ($("[data-fancybox='gallery']").length != 0)
@@ -272,10 +287,10 @@
     var minMenu = $(".header-scroll") || null;
     var headerRange = false;
     $(window).on("scroll", function(e) {
-      if ($(window).scrollTop() > 150 && headerRange == false) {
+      if ($(window).scrollTop() > 50 && headerRange == false) {
         headerRange = true;
         if (minMenu) minMenu.addClass("scrolled").addClass("down");
-      } else if ($(window).scrollTop() < 150 && headerRange == true) {
+      } else if ($(window).scrollTop() < 50 && headerRange == true) {
         headerRange = !true;
         if (minMenu) minMenu.removeClass("scrolled");
       } //.originalEvent.wheelDelta
@@ -358,12 +373,11 @@
 
 
 
-    window.revSlider = $('.rev-slider') || null;
+    var revSlider = $('.rev-slider') || null;
     var bannerSlider =  $('.rev-slider').hasClass("banner-slider") || null;
 
     onResized(function(){
-
-      if( revSlider )
+      if( revSlider.length != 0 )
        revSlider.revolution({
           delay:6000,
           startwidth: checkSm() ? $( window ).width() : checkMd() ? 970 : 1170,
@@ -460,7 +474,37 @@ setTimeout(function() {
     return this;
   };
 }, 10);
-
+String.prototype.unescape = function() {
+  var str;
+  var winalpha = {
+    E0: '%D0%B0', E1: '%D0%B1', E2: '%D0%B2', E3: '%D0%B3',
+    E4: '%D0%B4', E5: '%D0%B5', B8: '%D1%91', E6: '%D0%B6',
+    E7: '%D0%B7', E8: '%D0%B8', E9: '%D0%B9', EA: '%D0%BA',
+    EB: '%D0%BB', EC: '%D0%BC', ED: '%D0%BD', EE: '%D0%BE',
+    EF: '%D0%BF', F0: '%D1%80', F1: '%D1%81', F2: '%D1%82',
+    F3: '%D1%83', F4: '%D1%84', F5: '%D1%85', F6: '%D1%86',
+    F7: '%D1%87', F8: '%D1%88', F9: '%D1%89', FA: '%D1%8A',
+    FB: '%D1%8B', FC: '%D1%8C', FD: '%D1%8D', FE: '%D1%8E',
+    FF: '%D1%8F', C0: '%D0%90', C1: '%D0%91', C2: '%D0%92',
+    C3: '%D0%93', C4: '%D0%94', C5: '%D0%95', A8: '%D0%81',
+    C6: '%D0%96', C7: '%D0%97', C8: '%D0%98', C9: '%D0%99',
+    CA: '%D0%9A', CB: '%D0%9B', CC: '%D0%9C', CD: '%D0%9D',
+    CE: '%D0%9E', CF: '%D0%9F', D0: '%D0%A0', D1: '%D0%A1',
+    D2: '%D0%A2', D3: '%D0%A3', D4: '%D0%A4', D5: '%D0%A5',
+    D6: '%D0%A6', D7: '%D0%A7', D8: '%D0%A8', D9: '%D0%A9',
+    DA: '%D0%AA', DB: '%D0%AB', DC: '%D0%AC', DD: '%D0%AD',
+    DE: '%D0%AE', DF: '%D0%AF'
+  };
+  str = this.replace(/%/g, '$');
+  for (var i in winalpha) {
+    console.log(i); 
+     str = str.replace(new RegExp('[\$]'+i, 'g'), winalpha[i]);
+  }
+  console.log(str); 
+  str = str.replace(/\$/g, '%');
+  str = decodeURIComponent(str);
+  return str;
+}
 function checkSm() {
   return $(document).width() <= 991;
 }
